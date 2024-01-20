78c43839bff7be4920e0493186144bf9fedfe670
Samuel Wielgat <samuelwielgat@gmail.com>
2024-01-20 17:22:41 +0100

    Enable CORS for all routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
@@ -9,6 +10,7 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 // middleware
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
