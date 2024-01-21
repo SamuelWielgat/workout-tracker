0fa795f5f6e5b6dd2caf0c1e71fe902f6e886f93
Samuel Wielgat <samuelwielgat@gmail.com>
2024-01-21 16:14:49 +0100

    port config

--- backend/index.js
+++ backend/index.js
@@ -26,8 +26,15 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT);
+    // app.listen(process.env.PORT, () => {
+    //   console.log('connected to db & listening on port', process.env.PORT);
+    // });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(
+        'connected to db & listening on port',
+        process.env.PORT || 3000
+      );
     });
   })
   .catch((error) => {
